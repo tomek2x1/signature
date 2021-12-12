8d9dd48f4bf68a1da244f70997c6753fc40845b7
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-12-12 16:38:16 +0100

    zmiana położenia przycisku

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -19,13 +19,13 @@ const Btn = ({ icon, handle, name, type }) => {
 
 const MyButtonWrapper = styled("div")({
   textAlign: "right",
+  marginTop: 15,
+  marginBottom: 30,
 });
 
 const MyButton = styled(Button)({
   fontFamily: "'IBM Plex Sans', sans-serif",
   background: "linear-gradient(to right, #f07d3b, #ea5036)",
-  marginTop: 30,
-  marginBottom: 30,
 });
 
 export default Btn;
